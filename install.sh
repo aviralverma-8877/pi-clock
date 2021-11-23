26166988f21c6bbb3ca8ea47f2b3f5a44623fee4
Aviral Verma <sons2avitush@gmail.com>
2021-11-23 23:37:46 +0530

    Fix

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ apt-get install python3-pip -y
 apt-get install ttf-dejavu
 apt-get install python3-pil
 apt-get install python3-pil
+apt-get install npm -y
+npm install pm2 -g
 pip3 install adafruit-circuitpython-pcd8544
 pip3 install psutil
 pip3 install gpiozero
-pip3 install pm2
+pip3 install pm2 -g
 pm2 start main.py --interpreter python3
 pm2 save
 pm2 startup
