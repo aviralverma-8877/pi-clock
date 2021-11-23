0eb84bc06a31f057ab52ed0d8f1971e82d0b8ae7
Aviral Verma <sons2avitush@gmail.com>
2021-11-23 23:44:00 +0530

    fix

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ npm install pm2 -g
 pip3 install adafruit-circuitpython-pcd8544
 pip3 install psutil
 pip3 install gpiozero
-pip3 install pm2 -g
 pm2 start main.py --interpreter python3
 pm2 save
 pm2 startup
