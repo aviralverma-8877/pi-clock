cc7055fa2aeb7ad8fab015ec60f4f830b7e384c6
aviral.verma@wdc.com <aviral.verma@wdc.com>
2021-11-24 10:53:41 +0530

    Install Step Fix

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@
 apt update -y
 apt autoremove -y
 apt update --fix-missing -y
-apt install git python3-pip python3-pil npm -y
+apt install git python3-pip python3-pil -y
 git clone https://github.com/aviralverma-8877/pi-clock.git
 cd pi-clock
-npm install pm2 -g
 pip3 install adafruit-circuitpython-pcd8544
 pip3 install psutil
 pip3 install gpiozero
-pm2 start main.py --interpreter python3
-pm2 save
-pm2 startup
+touch /var/spool/cron/job
+echo '@reboot cd $PWD && python3 main.py' > /var/spool/cron/job
+reboot
