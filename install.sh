046e952f943aedcb5bdd5b99847eeba7465c88e7
aviral.verma@wdc.com <aviral.verma@wdc.com>
2021-11-24 11:26:21 +0530

    Install step updated

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ apt update -y
 apt autoremove -y
 apt update --fix-missing -y
 apt install git python3-pip python3-pil -y
-git clone https://github.com/aviralverma-8877/pi-clock.git
-cd pi-clock
 pip3 install adafruit-circuitpython-pcd8544
 pip3 install psutil
 pip3 install gpiozero
+git clone https://github.com/aviralverma-8877/pi-clock.git
+cd pi-clock
 crontab -l > crontab_new 
 echo "@reboot cd $PWD && python3 main.py" > crontab_new
 crontab crontab_new
